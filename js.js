7de6a982d2368f61b46cb8c31144fa24ae459a2b
parth thakor <parththakor2610@gmail.com>
2023-08-09 11:20:46 +0530

    second commit which gives product of two number and student object

--- js.js
+++ js.js
@@ -1,3 +1,18 @@
 const fs = require('fs');
 fs.writeFileSync('html.txt' , 'hello world');
 console.log('hellow world')
+
+const product = (a,b)=>{
+  return a*b;
+}
+const student ={
+  name :"rohan",
+  rollNo : 232 ,
+  greet(){
+    console.log(`Student-Name : ${this.name} and Roll-Number : ${this.rollNo}`)
+  }
+}
+console.log(product(2,3));
+console.log(student.name)
+console.log(student.rollNo)
+ student.greet();
